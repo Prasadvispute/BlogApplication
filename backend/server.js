cf7aa36080204eeacdb005b0110eb88aa2ef3021
Prasadvispute <143605728+Prasadvispute@users.noreply.github.com>
2024-08-21 20:39:46 +0530

    backend commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/posts', require('./routes/posts'));
+app.use('/api/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
